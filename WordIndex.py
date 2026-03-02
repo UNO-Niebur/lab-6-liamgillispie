a39cb625260a141cb79ef5dfa0362c8a876c630c
liamgillispie <l.gillispie526@gmail.com>
2026-03-02 06:06:48 +0000

    save

--- WordIndex.py
+++ WordIndex.py
@@ -5,25 +5,32 @@
 #Purpose: Build an index mapping each word to a list of line numbers where it appears
 
 def main():
-  textFile = open("fish.txt", 'r')
+  valid_files=["fish.txt","gettysberg.txt"]
+  fileName=input(f"Enter your chosen file here({', '.join(valid_files)}): (either 'gettysberg.txt' or 'fish.txt')")
   
-  words = {} #create an empty dictionary
-  lineNum=0
-  for line in textFile:
-    lineNum=lineNum+1
-    wordList=line.split()
-    for w in wordList:
-      w=w.lower()
-      w=w.replace(",", "")
-      w=w.replace(".", "")
-      if w in words:
-        if lineNum not in words[w]:
-          words[w].append(lineNum)
-      else:
-        words[w]=[lineNum]
-
-  for word in words:
-    print(words, words[word])
-
+  if fileName in valid_files:
+    try:
+      with open(fileName, 'r') as file:
+        print(f"Successfully opened {fileName}!")
+        words = {} #create an empty dictionary
+        lineNum=0
+        for line in file:
+          lineNum+=1
+          wordList=line.split()
+          for w in wordList:
+            w=w.lower()
+            w=w.replace(",", "")
+            w=w.replace(".", "")
+            if w in words:
+              if lineNum not in words[w]:
+                words[w].append(lineNum)
+            else:
+              words[w]=[lineNum]
+        for word in words:
+          print(f"Word:{word}, Line numbers:{words[word]}")
+    except FileNotFoundError:
+      print(f"Error: The file'{fileName}' was found in the list but not in the system.")
+  else:
+    print(f"Error:'{fileName}' is not a valid option.")
 if __name__ == '__main__':
   main()
